392515219af9b02a6fdf3cf471de6d847e1582f3
madiberikkazy004 <madiberikkazy004@gmail.com>
2023-06-07 14:19:19 +0600

    .

--- app/src/main/java/com/example/myapplication/create/presentation/viewmodel/CreateViewModel.kt
+++ app/src/main/java/com/example/myapplication/create/presentation/viewmodel/CreateViewModel.kt
@@ -1,16 +1,7 @@
 package com.example.myapplication.create.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.example.myapplication.create.domain.usecase.OpenDialogAddressOfMatch
-import com.example.myapplication.create.domain.usecase.OpenDialogDateOfMatch
-import com.example.myapplication.create.domain.usecase.OpenDialogTimeOfMatch
-import com.example.myapplication.create.domain.usecase.OpenDialogTypeOfSports
 
-class CreateViewModel(
-    private val openDialogTimeOfMatch: OpenDialogTimeOfMatch,
-    private val openDialogDateOfMatch: OpenDialogDateOfMatch,
-    private val openDialogAddressOfMatch: OpenDialogAddressOfMatch,
-    private val openDialogTypeOfSports: OpenDialogTypeOfSports
-) : ViewModel() {
+class CreateViewModel() : ViewModel() {
 
 }
